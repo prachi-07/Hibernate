6e8d40eebd82d78bdb28a97cbee8ea3562569b31
AE206_PC6 <AE206_PC6@Infva06186>
2018-08-10 18:02:10 +0530

    tenth

---
+++ Hibernated/src/lti/pojo/one2many/DeptEmpTest.java
@@ -0,0 +1,40 @@
+package lti.pojo.one2many;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.junit.Test;
+
+import lti.util.HibernateUtil;
+
+public class DeptEmpTest {
+	@Test
+	public void testCase1() {
+		Session session = HibernateUtil.getSession();
+		Transaction tx = session.beginTransaction();
+
+		Department dept = new Department();
+		dept.setDeptno(10);
+		dept.setName("Admin");
+		dept.setLocation("Andheri,Mumbai");
+
+		session.save(dept);
+		tx.commit();
+	}
+	
+	@Test
+	public void testCase2() {
+		Session session = HibernateUtil.getSession();
+		Transaction tx = session.beginTransaction();
+		
+		Department dept = (Department)session.get(Department.class, 10);
+		Employee emp = new Employee();
+		emp.setEmpno(1002);
+		emp.setName("Neha Padshala");
+		emp.setSalary(3000.0);
+		emp.setDept(dept);
+		
+		session.save(emp);
+		tx.commit();
+		
+	}
+}
